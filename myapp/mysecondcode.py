a53ac886c6bd31d5e20f14f278767d4645a1fb09
thelearner <thelearner360@gmail.com>
2019-07-20 17:04:19 +0000

    updated mysecondcode file

--- myapp/mysecondcode.py
+++ myapp/mysecondcode.py
@@ -6,6 +6,18 @@ num = 407
 
 if num > 1:
 
-for i in range(2,num):
-     if(num % i) == 0:
+  for i in range(2,num):
+      if(num % i) == 0:
         print(num,"is not a prime number")
+        break
+  else:
+
+     print(num,"is a prime number")
+
+# if the number is less than 1 it is not a prime
+else:
+
+     print(num,"is not a  prime number")
+ 
+
+  
