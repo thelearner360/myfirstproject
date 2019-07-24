92fe2901985e3bb064e2d14a332691ec2b12a9d3
thelearner360 <53090457+thelearner360@users.noreply.github.com>
2019-07-24 11:54:25 +0530

    updated the print stmts in mysecondcode.py

--- myapp/mysecondcode.py
+++ myapp/mysecondcode.py
@@ -12,16 +12,16 @@ if num > 1:
 
   for i in range(2,num):
       if(num % i) == 0:
-        print(num,"is not a prime number")
+        print(num 'is not a prime number')
         break
   else:
 
-     print(num,"is a prime number")
+     print(num  'is a prime number')
 
 # if the number is less than 1 it is not a prime
 else:
 
-     print(num,"is not a  prime number")
+     print(num 'is not a  prime number')
  
 
   
