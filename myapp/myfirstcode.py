c876ebd632f8f1fd553e6020f1fc87a7c8caab2d
thelearner360 <53090457+thelearner360@users.noreply.github.com>
2019-07-24 11:40:00 +0530

    updated myfirstcode.py to fix syntax error

--- myapp/myfirstcode.py
+++ myapp/myfirstcode.py
@@ -4,5 +4,5 @@ print('Hello World')
 
 print('update done by developer 1')
 
-print('updating from dev and master")
+print('updating from dev and master')
 # making a commit to test jenkins poll scm run 
